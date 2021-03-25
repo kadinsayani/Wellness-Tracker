b9dd2986adf0c8461222ccfd37f755a0abb967d2
Kadin Sayani <kadinsayani8@gmail.com>
2021-03-25 13:34:52 -0600

    formatted code

--- src/TrackerGUI.java
+++ src/TrackerGUI.java
@@ -4,7 +4,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.time.LocalDateTime;    
+import java.time.LocalDateTime;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -23,7 +23,7 @@ import javafx.scene.layout.VBox;
  *
  */
 public class TrackerGUI extends Application {
-	
+
 	HBox summarizeSteps;
 	HBox summarizeWater;
 	HBox summarizeSleep;
@@ -42,12 +42,11 @@ public class TrackerGUI extends Application {
 		// Create VBox and Scene
 		VBox root = new VBox();
 		Scene scene = new Scene(root, 400, 500);
-		
+
 		// Application styling
 		scene.getStylesheets().add(getClass().getResource("stylesheet.css").toExternalForm());
 
 		userRegistration(root);
-
 
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Wellness Tracker");
@@ -56,6 +55,7 @@ public class TrackerGUI extends Application {
 
 	/**
 	 * userRegistration method for getting info on user and creating user object
+	 * 
 	 * @param root
 	 */
 	public void userRegistration(VBox root) {
@@ -116,7 +116,8 @@ public class TrackerGUI extends Application {
 			root.getChildren().remove(myButton);
 			root.getChildren().removeAll(myLabel, hb1, hb2, hb3, hb4, hb5, myButton);
 
-			// now that user object is created we can remove the user reg text fields and call recordMetrics
+			// now that user object is created we can remove the user reg text fields and
+			// call recordMetrics
 			recordMetrics(root, user);
 		});
 		root.getChildren().add(myButton);
@@ -173,17 +174,17 @@ public class TrackerGUI extends Application {
 			alert.showAndWait();
 		});
 		calorieBox.getChildren().add(questionMark);
-		
+
 		summary.getChildren().addAll(stepBox, waterBox, sleepBox, exerciseBox, calorieBox);
 
 		root.getChildren().add(summary);
-		
-		
+
 		Button addButton = new Button("Add");
 		Tooltip tAddButton = new Tooltip("Click to submit entries. You can submit as many as you want at a time.");
 		Tooltip.install(addButton, tAddButton);
 		addButton.setOnAction((event) -> {
-			// button handler checks if user has inputted to text fields and if so adds user metric recordings to metricCount in metric object
+			// button handler checks if user has inputted to text fields and if so adds user
+			// metric recordings to metricCount in metric object
 			if (!(stepEntry.getText().isEmpty())) {
 				steps.recordMetric(Integer.parseInt(stepEntry.getText()));
 				stepCount.setText("Step count: " + String.valueOf(steps.getMetricCount()));
@@ -210,42 +211,43 @@ public class TrackerGUI extends Application {
 				calorieCount.setText("Calories: " + String.valueOf(calories.getCalorieCount()));
 				calorieEntry.clear();
 			}
-			
+
 			summarizeMetrics(root, steps, water, sleep, exercise, calories, user);
-			
+
 		});
-		
+
 		Button reportButton = new Button("Generate Report");
 		Tooltip tReportButton = new Tooltip("Click to generate a report on today's wellness metrics.");
 		Tooltip.install(reportButton, tReportButton);
 		reportButton.setOnAction((event) -> {
 			LocalDateTime now = LocalDateTime.now();
 			System.out.println("Wellness report generated " + now);
-			
+
 			// Create directory in home folder if not exists
-		    File directory = new File(File.listRoots()[0], "Users" + System.getProperty("file.separator") + System.getProperty("user.name") + System.getProperty("file.separator") + "Wellness Tracker Reports");
-		    if (!directory.exists()) {
-		    	directory.mkdir();
-		    }
-		    
-		    // Create report txt file in Wellness Tracker Reports directory
-		    String fileName = "Wellness Report " + now + ".txt";
-		    File file = new File(directory + "/" + fileName);
-		    try{
-		        FileWriter fw = new FileWriter(file.getAbsoluteFile());
-		        BufferedWriter bw = new BufferedWriter(fw);
-		        bw.write("Wellness Report generated " + now + "\n");
-		        bw.write("User: " + user.getName() + "\n");
-		        bw.write("Steps: " + steps.getMetricCount() + " steps\n");
-		        bw.write("Water: " + water.getMetricCount() + " fluid ounces\n");
-		        bw.write("Sleep: " + sleep.getMetricCount() + " hours\n");
-		        bw.write("Exercise: " + exercise.getMetricCount() + " minutes\n");
-		        bw.write("Calories: " + calories.getCalorieCount() + " calories\n");
-		        bw.close();
-		    }
-		    catch (IOException e){
-		        e.printStackTrace();
-		    }
+			File directory = new File(File.listRoots()[0],
+					"Users" + System.getProperty("file.separator") + System.getProperty("user.name")
+							+ System.getProperty("file.separator") + "Wellness Tracker Reports");
+			if (!directory.exists()) {
+				directory.mkdir();
+			}
+
+			// Create report txt file in Wellness Tracker Reports directory
+			String fileName = "Wellness Report " + now + ".txt";
+			File file = new File(directory + "/" + fileName);
+			try {
+				FileWriter fw = new FileWriter(file.getAbsoluteFile());
+				BufferedWriter bw = new BufferedWriter(fw);
+				bw.write("Wellness Report generated " + now + "\n");
+				bw.write("User: " + user.getName() + "\n");
+				bw.write("Steps: " + steps.getMetricCount() + " steps\n");
+				bw.write("Water: " + water.getMetricCount() + " fluid ounces\n");
+				bw.write("Sleep: " + sleep.getMetricCount() + " hours\n");
+				bw.write("Exercise: " + exercise.getMetricCount() + " minutes\n");
+				bw.write("Calories: " + calories.getCalorieCount() + " calories\n");
+				bw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		});
 
 		root.getChildren().add(addButton);
@@ -254,7 +256,8 @@ public class TrackerGUI extends Application {
 	}
 
 	/**
-	 * summarizeMetrics provides charts for summarizing metrics and how far a user is from their goals
+	 * summarizeMetrics provides charts for summarizing metrics and how far a user
+	 * is from their goals
 	 * 
 	 * @param root
 	 * @param steps
@@ -264,10 +267,12 @@ public class TrackerGUI extends Application {
 	 * @param calories
 	 * @param user
 	 */
-	public void summarizeMetrics(VBox root, Metric steps, Metric water, Metric sleep, Metric exercise, Metric calories, User user) {
-		
-		root.getChildren().removeAll(summarizeSteps, summarizeWater, summarizeSleep, summarizeExercise, summarizeCalories);
-		
+	public void summarizeMetrics(VBox root, Metric steps, Metric water, Metric sleep, Metric exercise, Metric calories,
+			User user) {
+
+		root.getChildren().removeAll(summarizeSteps, summarizeWater, summarizeSleep, summarizeExercise,
+				summarizeCalories);
+
 		summarizeSteps = new HBox();
 		Label s1 = new Label("% of step goal reached");
 		ProgressBar stepProgress = new ProgressBar(0);
@@ -275,23 +280,25 @@ public class TrackerGUI extends Application {
 		stepProgress.setProgress(steps.getMetricCount() / user.getStepGoal());
 		Label s2 = new Label(Math.round((steps.getMetricCount() / user.getStepGoal()) * 100) + "%");
 		summarizeSteps.getChildren().addAll(s1, stepProgress, s2);
-		
+
 		summarizeWater = new HBox();
 		Label w1 = new Label("% of water intake goal reached");
 		ProgressBar waterProgress = new ProgressBar(0);
 		waterProgress.setPadding(new Insets(5.0, 5.0, 5.0, 5.0));
 		waterProgress.setProgress(water.getMetricCount() / water.calculateRequiredWaterIntake(user));
-		Label w2 = new Label(Math.round((water.getMetricCount() / water.calculateRequiredWaterIntake(user)) * 100) + "%");
+		Label w2 = new Label(
+				Math.round((water.getMetricCount() / water.calculateRequiredWaterIntake(user)) * 100) + "%");
 		summarizeWater.getChildren().addAll(w1, waterProgress, w2);
-		
+
 		summarizeSleep = new HBox();
 		Label sl1 = new Label("% of sleep goal reached");
 		ProgressBar sleepProgress = new ProgressBar(0);
 		sleepProgress.setPadding(new Insets(5.0, 5.0, 5.0, 5.0));
 		sleepProgress.setProgress(sleep.getMetricCount() / sleep.calculateRecommendedSleep(user.getAge()));
-		Label sl2 = new Label(Math.round((sleep.getMetricCount() / sleep.calculateRecommendedSleep(user.getAge())) * 100) + "%");
+		Label sl2 = new Label(
+				Math.round((sleep.getMetricCount() / sleep.calculateRecommendedSleep(user.getAge())) * 100) + "%");
 		summarizeSleep.getChildren().addAll(sl1, sleepProgress, sl2);
-		
+
 		summarizeExercise = new HBox();
 		Label e1 = new Label("% of exercise minutes goal reached");
 		ProgressBar exerciseProgress = new ProgressBar(0);
@@ -299,7 +306,7 @@ public class TrackerGUI extends Application {
 		exerciseProgress.setProgress(exercise.getMetricCount() / 60.0);
 		Label e2 = new Label(Math.round((exercise.getMetricCount() / 60.0) * 100) + "%");
 		summarizeExercise.getChildren().addAll(e1, exerciseProgress, e2);
-		
+
 		summarizeCalories = new HBox();
 		Label c1 = new Label("% of calorie goal reached");
 		ProgressBar calorieProgress = new ProgressBar(0);
@@ -307,9 +314,9 @@ public class TrackerGUI extends Application {
 		calorieProgress.setProgress(calories.getCalorieCount() / user.getCalorieGoal());
 		Label c2 = new Label(Math.round((calories.getCalorieCount() / user.getCalorieGoal()) * 100) + "%");
 		summarizeCalories.getChildren().addAll(c1, calorieProgress, c2);
-		
+
 		root.getChildren().addAll(summarizeSteps, summarizeWater, summarizeSleep, summarizeExercise, summarizeCalories);
-		
+
 	}
 
 }
