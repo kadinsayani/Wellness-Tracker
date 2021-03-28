18543cd1d99921cc06be92b747b0adc47cc29747
Kadin Sayani <kadinsayani8@gmail.com>
2021-03-28 15:13:19 -0600

    user can now switch between text based ui and gui

--- src/TextInterface.java
+++ src/TextInterface.java
@@ -24,22 +24,22 @@ public class TextInterface {
 		Metric exercise = new Metric("exercise");
 		Metric calories = new Metric("calories");
 
-		Scanner menuSc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
 		boolean menuActive = true;
 		
 		System.out.println(
 				"Welcome to Wellness Tracker! Wellness Tracker records steps, water intake, time spent sleeping, and also does food tracking!");
 		System.out.println("Please enter your name: ");
-		String name = menuSc.nextLine();
+		String name = sc.nextLine();
 		System.out.println("Please enter your age: ");
-		int age = menuSc.nextInt();
+		int age = sc.nextInt();
 		System.out.println("Please enter your weight in lb: ");
-		int weight = menuSc.nextInt();
+		int weight = sc.nextInt();
 		System.out.println("Please enter your step goal: ");
-		int stepGoal = menuSc.nextInt();
+		int stepGoal = sc.nextInt();
 		System.out.println("Please enter your calorie goal: ");
-		int calorieGoal = menuSc.nextInt();
+		int calorieGoal = sc.nextInt();
 		System.out.println("\n");
 
 		// Create user object and pass in user inputed data
@@ -69,25 +69,36 @@ public class TextInterface {
 			System.out.println("5: Record food");
 			System.out.println("6: Exit application");
 
-			String menuSelection = menuSc.nextLine();
+			String menuSelection = sc.nextLine();
 
 			// Switch statement for menu option selection
 
 			switch (menuSelection) {
 			case "1":
-				//todo
+				System.out.println("Please enter the amount of steps you  would like to record: ");
+				int stepsToAdd = sc.nextInt();
+				steps.recordMetric(stepsToAdd);
 				break;
 			case "2":
-				//todo
+				System.out.println("Please enter the amount of water (fluid ounces) you  would like to record: ");
+				int waterToAdd = sc.nextInt();
+				water.recordMetric(waterToAdd);
 				break;
 			case "3":
-				//todo
+				System.out.println("Please enter the amount of sleep (hours) you  would like to record: ");
+				int sleepToAdd = sc.nextInt();
+				sleep.recordMetric(sleepToAdd);
 				break;
 			case "4":
-				//todo
+				System.out.println("Please enter the amount of exercise (minutes) you  would like to record: ");
+				int exerciseToAdd = sc.nextInt();
+				exercise.recordMetric(exerciseToAdd);
 				break;
 			case "5":
-				//todo
+				FoodAPI f = new FoodAPI();
+				System.out.println("Please enter a food item you would like to record: ");
+				String foodItemToAdd = sc.nextLine();
+				calories.addToCalorieCount(f.requestCalories(foodItemToAdd));
 				break;
 			case "6":
 				menuActive = false;
